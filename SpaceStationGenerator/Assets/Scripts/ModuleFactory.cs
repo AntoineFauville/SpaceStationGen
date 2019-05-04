5721df690146623ca084303d77eb76d55ed66eb7
sheido <af8081@gmail.com>
2019-05-04 12:35:27 +0200

    trying out with connections

--- SpaceStationGenerator/Assets/Scripts/ModuleFactory.cs
+++ SpaceStationGenerator/Assets/Scripts/ModuleFactory.cs
@@ -5,12 +5,55 @@ public class ModuleFactory
 {
     [Inject] private GameSettings _gameSettings;
 
-    public Module CreateModule(Vector3 position, Transform parent)
+    public Module CreateModule(Connection connection, Transform parent)
     {
         Module module = Object.Instantiate(_gameSettings.Module);
+      
+        module.name = "X " + connection.transform.position.x.ToString() + " Y " + connection.transform.position.y.ToString() + " Z " + connection.transform.position.z.ToString();
+        module.SetupReferences(_gameSettings);
+        module.transform.SetParent(parent);
+
+        module.transform.localRotation = connection.transform.localRotation;
+        //find the positioon of the next module + offset
+        Vector3 pos = connection.MyModule.transform.localPosition;
+        Vector3 offset = new Vector3();
+        if (connection.transform.position.x > pos.x)
+        {
+            offset = pos + new Vector3(7, 0, 0);
+        }
+        else if (connection.transform.position.x < pos.x)
+        {
+            offset = pos + new Vector3(-7, 0, 0);
+        }
+        else if (connection.transform.position.y > pos.y)
+        {
+            offset = pos + new Vector3(0, 7, 0);
+        }
+        else if (connection.transform.position.y < pos.y)
+        {
+            offset = pos + new Vector3(0, -7, 0);
+        }
+        else if (connection.transform.position.z > pos.z)
+        {
+            offset = pos + new Vector3(0, 0, 7);
+        }
+        else if (connection.transform.position.z < pos.z)
+        {
+            offset = pos + new Vector3(0, 0, -7);
+        }
+        module.transform.localPosition = offset;
+        return module;
+    }
+
+    public Module CreateStartingModule(Vector3 position, Transform parent)
+    {
+        Module module = Object.Instantiate(_gameSettings.Module);
+
+        module.name = "X " + position.x.ToString() + " Y " + position.y.ToString() + " Z " + position.z.ToString();
         module.SetupReferences(_gameSettings);
         module.transform.SetParent(parent);
         module.transform.position = position;
+
         return module;
     }
 }
--- SpaceStationGenerator/Assets/Scripts/Structure.cs
+++ SpaceStationGenerator/Assets/Scripts/Structure.cs
@@ -7,71 +7,86 @@ public class Structure : MonoBehaviour
 {
     [Inject] private ModuleFactory ModuleFactory;
     [Inject] private GameSettings _gameSettings;
-    
-    public Vector3 currentPosition;
 
-    List<Vector3> savedPosition = new List<Vector3>();
-
-    private bool destroyEntity;
+    List<Module> modules = new List<Module>();
+    List<Connection> freeConnections = new List<Connection>();
 
     void Start()
     {
-        //base module
-        ModuleFactory.CreateModule(_gameSettings.initialPosition, this.transform);
-        currentPosition = _gameSettings.initialPosition;
-        savedPosition.Add(currentPosition);
+        //base module creation
+        Module baseModule = ModuleFactory.CreateStartingModule(_gameSettings.initialPosition, this.transform);
+        modules.Add(baseModule);
+        //assign the parent module to the connection
+        for (int i = 0; i < baseModule.Connections.Length; i++)
+        {
+            baseModule.Connections[i].MyModule = baseModule;
+        }
 
-        //get a random rotation and add one
         for (int i = 0; i < _gameSettings.ModuleAmount; i++)
         {
-            Vector3 pos = GenerateNewPosition();
-            if (!destroyEntity)
+            //get all the free connections
+            GetFreeConnections();
+            //get all the free space where i can create a new module, choose randomly between free connection, and place the module
+            Connection currentConnection = freeConnections[Random.Range(0, freeConnections.Count)];
+        
+            //generate the module
+            Module node = ModuleFactory.CreateModule(currentConnection, this.transform);
+            //assign the parent module to the connection
+            for (int y = 0; y < node.Connections.Length; y++)
             {
-                ModuleFactory.CreateModule(pos, this.transform);
+                node.Connections[y].MyModule = node;
             }
-            else
-            {
+            currentConnection.ConnectedModule = node;
+            modules.Add(node);
+            
+        }
 
-                destroyEntity = false;
+        for (int i = 0; i < modules.Count; i++)
+        {
+            PlaceConnectionsVisuals(modules[i]);
+            PlaceStorageBoxes(modules[i]);
+        }
+       
+    }
+    
+    public void GetFreeConnections()
+    {
+        for (int i = 0; i < modules.Count; i++)
+        {
+            for (int y = 0; y < modules[i].Connections.Length; y++)
+            {
+                if(modules[i].Connections[y].ConnectedModule == null)
+                {
+                    freeConnections.Add(modules[i].Connections[y]);
+                }
             }
         }
     }
 
-    public Vector3 GenerateNewPosition()
+    public void PlaceConnectionsVisuals(Module module)
     {
-        int direction = Random.Range(1, 4);
-        if (direction == 1)
+        for (int i = 0; i < module.Connections.Length; i++)
         {
-            return ModulePosition(new Vector3(7,0,0));            
+            if (module.Connections[i].ConnectedModule != null)
+            {
+                GameObject connection = Object.Instantiate(_gameSettings.Connection);
+                connection.transform.SetParent(module.Connections[i].transform);
+                connection.transform.rotation = module.Connections[i].transform.rotation;
+                connection.transform.localPosition = new Vector3(0, 0, 0 + _gameSettings.ConnectionOffset);
+            }
         }
-        if (direction == 2)
-        {
-            return ModulePosition(new Vector3(-7, 0, 0));
-        }
-        if (direction == 3)
-        {
-            return ModulePosition(new Vector3(0, 0, 7));
-        }
-        if (direction == 4)
-        {
-            return ModulePosition(new Vector3(0, 0, -7));
-        }
-        return new Vector3(0,0,0);
     }
 
-    private Vector3 ModulePosition(Vector3 position)
+    public void PlaceStorageBoxes(Module module)
     {
-        Vector3 modulePosition = currentPosition + new Vector3(position.x, position.y, position.z);
-        if (savedPosition.Contains(modulePosition))
+        for (int i = 0; i < module.StorageBoxes.Length; i++)
         {
-            destroyEntity = true;
-            return new Vector3(0, 0, 0);
-        }
-        else
-        {
-            currentPosition = modulePosition;
-            savedPosition.Add(currentPosition);
-            return modulePosition;
+            if (_gameSettings.BoxChance > Random.Range(0, 100))
+            {
+                GameObject storage = Object.Instantiate(_gameSettings.StorageBox);
+                storage.transform.SetParent(module.StorageBoxes[i].transform);
+                storage.transform.localPosition = new Vector3(0, 0, 0);
+            }
         }
     }
 }
